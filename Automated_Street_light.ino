624928d5859ca23e607c55086560021f0c6b3478
Ninad Rajesh Topale <topaleninad01@gmail.com>
2021-10-14 07:49:53 +0530

    Create Automated_Street_light.ino

---
+++ Automated_Street_light.ino
@@ -0,0 +1,16 @@
+int ldr=A5;
+int ldr_value;
+int light=3;
+void setup()
+{
+  pinMode(light, OUTPUT);
+  pinMode(ldr, INPUT);
+}
+void loop()
+{
+  ldr_value=analogRead(ldr);
+  if (ldr_value>512)
+    digitalWrite(light, LOW);
+  else
+    digitalWrite(light, HIGH);
+}
